e25e836d6960f658e4e98d0a70d0210e7d17b84c
Dang <ducdangnguyen441@gmail.com>
2023-04-28 09:47:38 +0700

    update from hiep, dang

--- Khach155/Program.cs
+++ Khach155/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddSession(options =>
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
+
 builder.Services.AddSession(options =>
 {
 	options.IdleTimeout = TimeSpan.FromMinutes(30);
